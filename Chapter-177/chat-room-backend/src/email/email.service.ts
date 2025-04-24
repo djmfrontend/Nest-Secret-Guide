c82bbb332cbef4163efdb825b355ef4d8d9c4921
JiaMing Ding <djmfrontend@163.com>
2025-04-24 16:25:57 +0800

    update

--- Chapter-177/chat-room-backend/src/email/email.service.ts
+++ Chapter-177/chat-room-backend/src/email/email.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create-email.dto';
 import { UpdateEmailDto } from './dto/update-email.dto';
+import { createTransport, Transporter } from 'nodemailer';
 
 @Injectable()
 export class EmailService {
+  transporter: Transporter;
+  constructor() {
+    this.transporter = createTransport({
+      host: 'smtp.qq.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: '1024618744@qq.com',
+        pass: 'rcxtvsdpdtfqbcbe',
+      },
+    });
+    //
+  }
+  async sendEmail({ to, subject, html }) {
+    await this.transporter.sendMail({
+      from: {
+        name: '聊天室',
+        address: '1024618744@qq.com',
+      },
+      to,
+      subject,
+      html,
+    });
+  }
   create(createEmailDto: CreateEmailDto) {
     return 'This action adds a new email';
   }
--- Chapter-177/chat-room-backend/src/user/user.controller.ts
+++ Chapter-177/chat-room-backend/src/user/user.controller.ts
@@ -7,38 +7,56 @@ import {
   Param,
   Delete,
   Inject,
+  Query,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { RedisService } from 'src/redis/redis.service';
+import { EmailService } from 'src/email/email.service';
 
 @Controller('user')
 export class UserController {
+  @Inject(RedisService)
+  private readonly redisService: RedisService;
+  @Inject(EmailService)
+  private readonly emailService: EmailService;
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
   async register(@Body() registerUser: RegisterUserDto) {
     return await this.userService.register(registerUser);
   }
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Get('register-captcha')
+  async captha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+    await this.redisService.set(`captcha_${address}`, code, 5 * 60);
+    await this.emailService.sendEmail({
+      to: address,
+      subject: '注册验证码',
+      html: `<p>你的注册验证码是 ${code}</p>`,
+    });
+    return '发送成功';
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
+  // @Get()
+  // findAll() {
+  //   return this.userService.findAll();
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
 }
