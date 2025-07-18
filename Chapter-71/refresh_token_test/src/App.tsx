fea3a82e3d7669e3edc50c182b81831357282157
JiaMing Ding <djmfrontend@163.com>
2025-07-18 16:06:23 +0800

    update

--- Chapter-71/refresh_token_test/src/App.tsx
+++ Chapter-71/refresh_token_test/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  axios.interceptors.request.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      // 如果accessToken 过期 则请求refresh 接口 获取新的accessToken 如果refreshToken  这跳转至登录页
+      let { data, config } = error.response;
+      if (data.statusCode === 401) {
+        //
+      }
+    }
   );
+  const login = async () => {
+    const res = await axios.post("http://localhost:3071/user/login", {
+      username: "root",
+      password: "123456",
+    });
+    localStorage.setItem("accessToken", res.data.accessToken);
+    localStorage.setItem("refreshToken", res.data.refreshToken);
+  };
+
+  const query = async () => {
+    await login();
+    const res = await axios.get("http://localhost:3071/user/aaa");
+    const resb = await axios.get("http://localhost:3071/user/bbb", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    });
+    console.log(res.data);
+  };
+  useEffect(() => {
+    query();
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
